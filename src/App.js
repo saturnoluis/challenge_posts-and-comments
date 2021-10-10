443d717af6881b1e5893889a7acc3fdffb3ab553
Luis Saturno <saturno.luis@gmail.com>
2021-10-10 18:50:15 -0300

    Install & implement react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import Home from './pages/Home';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
