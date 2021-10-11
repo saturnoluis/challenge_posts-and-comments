9484906391d31c6260ea6465972613b9292c0c03
Luis Saturno <saturno.luis@gmail.com>
2021-10-11 15:18:21 -0300

    Add loading state to feed slice

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,12 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { fetchPosts, selectPosts } from '../../store/slice/feed';
+import { fetchPosts, selectLoading, selectPosts } from '../../store/slice/feed';
 import Feed from "../../components/Feed";
 
 export default function Home () {
   const posts = useSelector(selectPosts);
+  const loading = useSelector(selectLoading);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -14,7 +15,10 @@ export default function Home () {
 
   return (
     <main data-testid="Home" role="main">
-      <Feed posts={posts} />
+      {loading
+        ? <h1>Loading...</h1>
+        : <Feed posts={posts} />
+      }
     </main>
   );
 }
--- src/store/slice/feed.js
+++ src/store/slice/feed.js
@@ -10,26 +10,34 @@ const slice = createSlice({
   name: 'feed',
   initialState: {
     posts: [],
+    loading: false,
   },
   reducers: {
     setPosts: (state, action) => {
       state.posts = action.payload;
     },
+    toggleLoading: state => {
+      state.loading = !state.loading;
+    }
   },
 });
 
 // Selectors
 export const selectPosts = state => state.feed.posts;
+export const selectLoading = state => state.feed.loading;
 
 // Thunks
 export const fetchPosts = () => dispatch => {
+  dispatch(toggleLoading());
+
   setTimeout(() => {
     dispatch(setPosts(dummyPosts));
+    dispatch(toggleLoading());
   }, 2000);
 };
 
 // Actions
-export const { setPosts } = slice.actions;
+export const { setPosts, toggleLoading } = slice.actions;
 
 // Reducer
 export default slice.reducer;
